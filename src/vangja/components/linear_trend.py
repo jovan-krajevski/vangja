e46b8e5f05a3ac278a989270c10e80701e361ade
Jovan Krajevski <jovan.krajevski@gmail.com>
2025-02-18 20:08:29 +0100

    Speed up find_MAP and fix 0 changepoints bug in LinearTrend

--- src/vangja/components/linear_trend.py
+++ src/vangja/components/linear_trend.py
@@ -35,64 +35,67 @@ class LinearTrend(TimeSeriesModel):
         model_idxs["lt"] += 1
 
         with model:
+            t = np.array(data["t"])
             slope = pm.Normal(
                 f"lt_{self.model_idx} - slope", self.slope_mean, self.slope_sd
             )
-
-            delta_sd = self.delta_sd
-            if self.delta_sd is None:
-                delta_sd = pm.Exponential(f"lt_{self.model_idx} - tau", 1.5)
-
-            delta = pm.Laplace(
-                f"lt_{self.model_idx} - delta",
-                self.delta_mean,
-                delta_sd,
-                shape=self.n_changepoints,
-            )
-
             intercept = pm.Normal(
                 f"lt_{self.model_idx} - intercept",
                 self.intercept_mean,
                 self.intercept_sd,
             )
 
-            t = np.array(data["t"])
-            hist_size = int(np.floor(data.shape[0] * self.changepoint_range))
-            cp_indexes = (
-                np.linspace(0, hist_size - 1, self.n_changepoints + 1)
-                .round()
-                .astype(int)
-            )
-            self.s = np.array(data.iloc[cp_indexes]["t"].tail(-1))
-            A = (t[:, None] > self.s) * 1
+            if self.n_changepoints > 0:
+                delta_sd = self.delta_sd
+                if self.delta_sd is None:
+                    delta_sd = pm.Exponential(f"lt_{self.model_idx} - tau", 1.5)
 
-            gamma = -self.s * delta
-            trend = (slope + pm.math.sum(A * delta, axis=1)) * t + (
-                intercept + pm.math.sum(A * gamma, axis=1)
-            )
+                delta = pm.Laplace(
+                    f"lt_{self.model_idx} - delta",
+                    self.delta_mean,
+                    delta_sd,
+                    shape=self.n_changepoints,
+                )
+
+                hist_size = int(np.floor(data.shape[0] * self.changepoint_range))
+                cp_indexes = (
+                    np.linspace(0, hist_size - 1, self.n_changepoints + 1)
+                    .round()
+                    .astype(int)
+                )
+                self.s = np.array(data.iloc[cp_indexes]["t"].tail(-1))
+                A = (t[:, None] > self.s) * 1
+
+                gamma = -self.s * delta
+                trend = (slope + pm.math.sum(A * delta, axis=1)) * t + (
+                    intercept + pm.math.sum(A * gamma, axis=1)
+                )
+            else:
+                trend = slope * t + intercept
 
         return trend
 
     def _tune(self, model, data, initvals, model_idxs, prev):
         return self.definition(model, data, initvals, model_idxs)
 
-    def _set_initval(self, initvals, model: pm.Model):
+    def _get_initval(self, initvals, model: pm.Model):
         slope_initval = initvals.get("slope", None)
         intercept_initval = initvals.get("intercept", None)
         delta_initval = initvals.get("delta", None)
         if delta_initval is not None:
             delta_initval = np.array([delta_initval] * self.n_changepoints)
 
-        model.set_initval(
-            model.named_vars[f"lt_{self.model_idx} - slope"], slope_initval
-        )
-        model.set_initval(
-            model.named_vars[f"lt_{self.model_idx} - intercept"], intercept_initval
-        )
+        initval_dict = {
+            model.named_vars[f"lt_{self.model_idx} - slope"]: slope_initval,
+            model.named_vars[f"lt_{self.model_idx} - intercept"]: intercept_initval,
+        }
+
         if f"lt_{self.model_idx} - delta" in model.named_vars:
-            model.set_initval(
-                model.named_vars[f"lt_{self.model_idx} - delta"], delta_initval
+            initval_dict[model.named_vars[f"lt_{self.model_idx} - delta"]] = (
+                delta_initval
             )
+
+        return initval_dict
 
     def _predict_map(self, future, map_approx):
         if f"lt_{self.model_idx} - delta" not in map_approx:
